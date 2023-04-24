55b378be685537b7a8fcb140472bf2f4d2ca986e
Thomas <nyumat18@gmail.com>
2023-04-23 20:08:40 -0700

    Added Infinite Scroll Support to fetchers

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -1,5 +1,6 @@
 import ky from "ky"
 import { json } from "react-router-dom"
+import { PAGE_LIMIT, PokemonResponse } from '../types/Pokemon';
 
 const BASE_URL = 'https://pokeapi.co/api/v2'
 
@@ -7,9 +8,17 @@ export const pokemonFetcher = (id: string | number) =>
       ky.get(`${BASE_URL}/pokemon/${id}`)
             .then(res => res.json())
 
-export const allPokemon = () =>
-      ky.get(`${BASE_URL}/pokemon?limit=40`)
-            .then(res => res.json())
+export const allPokemon = async ({ pageParam = 0 }): Promise<PokemonResponse> => {
+      const response: PokemonResponse = await ky
+            .get(`https://pokeapi.co/api/v2/pokemon?limit=${PAGE_LIMIT}&offset=${pageParam}`)
+            .json();
+      return response;
+};
+
+export const evolutionChainFetcher = async (url: string) => {
+      let response: any = await ky.get(url)
+      response = await response.json()
+}
 
 export const singleFetcher = async (url: string) => {
       let response: any = await ky.get(url)
@@ -17,3 +26,9 @@ export const singleFetcher = async (url: string) => {
       return response;
 }
 
+export const searchFetcher = async (search: string) => {
+      let response: any = await ky.get(`${BASE_URL}/pokemon/${search}`)
+      response = await response.json()
+      console.log(response)
+      return response;
+}
